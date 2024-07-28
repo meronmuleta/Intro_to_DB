1dae67ae9235f1193baae09141e8f433e512891d
Meron <meronmuleta25@gmail.com>
2024-07-28 10:46:19 -0700

    create DB

---
+++ MySQLServer.py
@@ -0,0 +1,13 @@
+import mysql.connector
+from mysql.connector import errorcode
+mydb = mysql.connector.connect(host = "localhost",user = "root",passwd = "1234")
+mycursor = mydb.cursor()
+try:
+    mycursor.execute('CREATE DATABASE alx_book_store')
+except mysql.connector.Error as e:
+    if e.errno == errorcode.ER_DB_CREATE_EXISTS:
+        print("Database 'alx_book_store' already exists.")
+else:
+    print("Database 'alx_book_store' created successfully!")
+mycursor.close()
+mydb.close()
